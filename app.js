02f65405a37aba9f9fbfd291df9551944dd35117
George <bl1723r@gmail.com>
2019-03-11 21:11:11 +0200

    init

---
+++ app.js
@@ -0,0 +1,18 @@
+const panels = document.querySelectorAll('.panel');
+
+// Event listeners
+panels.forEach(panel => panel.addEventListener('click', toggleOpen));
+panels.forEach(panel => panel.addEventListener('transitionend', toggleActive));
+
+// Toggle flex item resize
+function toggleOpen() {
+    this.classList.toggle('open');
+}
+
+// Toggle text animation
+function toggleActive(e) {
+    // Listen for "flex-grow" ("flex" in Safari, so "includes" used to cover both)
+    if (e.propertyName.includes('flex')) {
+        this.classList.toggle('open-active');
+    }
+}
