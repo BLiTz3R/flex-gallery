bc302c9a32dd1ccb169ce4afd908ad82e6e5f6ce
George <bl1723r@gmail.com>
2019-03-22 16:27:31 +0200

    update

--- app.js
+++ app.js
@@ -4,19 +4,6 @@ const panels = document.querySelectorAll('.panel');
 panels.forEach(panel => panel.addEventListener('click', toggleOpen));
 panels.forEach(panel => panel.addEventListener('transitionend', toggleActive));
 
-    // Show loader
-    document.querySelectorAll('.panel').style.opacity = '0.1';
-
-document.addEventListener('readystatechange', event => {
-
-    if (event.target.readyState === "complete") {
-        alert("Now external resources are loaded too, like css,src etc... ");
-                // Hide loader
-                document.querySelectorAll('.panel').style.opacity = '1';
-    }
-
-});
-
 // Toggle flex item resize
 function toggleOpen() {
     this.classList.toggle('open');
